25511ce6abc4ba391e1fd40d22b9f94fd527182e
Diego Gilson <diego.gilson@student.vinci.be>
2021-12-06 14:26:17 +0100

    creation de jeuRouter.js et tous les requetes router

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
-
 var authsRouter = require("./routes/auths");
+var jeuRouter = require("./routes/jeuRouter");
+
+
+
+
 
 
 var app = express();
@@ -13,7 +16,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use("/auths", authsRouter);
 
